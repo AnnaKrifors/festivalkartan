cdc8843139b61a1640b4c5d1199b9401b6f93b57
AnnaKrifors <113009629+AnnaKrifors@users.noreply.github.com>
2024-01-08 12:40:40 +0100

    Tailwindcolors (#12)
    * changed the tailwind colors for the new design
    
    * Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>
    
    put the colors from figma into tailwind
    
    * added #
    
    Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>
    
    * changed to snake case on markertype
    
    ---------
    
    Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,17 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: "rgb(35, 31, 32)",
-        brown: "rgb(168, 78, 64)",
-        blue: "rgb(0, 62, 74)",
-        "dark-pink": "rgb(242, 130, 127)",
-        green: "rgb(68, 99, 75)",
-        pink: "rgb(247, 161, 161)",
-        teal: "rgb(124, 204, 191)",
-        white: "rgb(255, 255, 255)",
-        yellow: "rgb(255, 224, 117)",
-        "dark-green": "#216b5e",
-        beige: "#fff8d6",
+        eggwhite: "#FCFCFC",
+        limegreen: "#B5D9B9",
+        white: "#FFFFFF",
+        black: "#1B1B1B",
+        cherryred: "#C03743",
+        darkgrey: "#BCBCBC",
+        lightgrey: "#00000029",
+        darkgreen: "#216B5B",
+        coralred: "#E9425C",
       },
       dropShadow: {
         markerShadow: "0 0px 15px rgba(255, 255, 255, 0.93)",
