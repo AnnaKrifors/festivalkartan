71998f1041d362fa7e74f6d90583c043c72dac08
Anton Landor <anton.landor@consid.se>
2024-02-09 13:12:27 +0100

    fix: Improve modal title

--- src/lib/Modal/ExpandButton.svelte
+++ src/lib/Modal/ExpandButton.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <button
-  title={`${expanded ? "minimera" : "expandera"} modal`}
-  class="bg-darkgreen absolute left-1/2 top-0 -translate-x-1/2 rounded-full p-2 lg:hover:bg-black"
+  title={`${expanded ? "minimera" : "expandera"} meny`}
+  class="absolute left-1/2 top-0 -translate-x-1/2 rounded-full bg-darkgreen p-2 lg:hover:bg-black"
   on:click={onClick}
 >
   <Icon
