e5612c927fb50ab77862a5b64e9f29e5133dbf91
Isac Zetterström <113009613+IsacZetterstrom@users.noreply.github.com>
2024-02-21 13:09:43 +0100

    Feature/105 historyback (#66)
    * user is able to navigate with the browsers back / forward button now.
    
    works mostly, a bug left that needs to be checked.
    
    * implemented a fix after last commit - it should now find the correct place every time (thats a good feature... lol)

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
   import Map from "./lib/Map.svelte";
   import Modal from "./lib/Modal/Modal.svelte";
+  import { setCurrentPlace, unsetCurrentPlace } from "./utils/marker";
+  import { placesStore } from "./data/places";
+
+  window.addEventListener("popstate", () => {
+    const matchedPlace = $placesStore.find((place) => {
+      const formattedTitle = place.postTitle.toLowerCase();
+
+      const formattedURL = decodeURIComponent(window.location.href)
+        .substring(
+          decodeURIComponent(window.location.href).indexOf("?place=") + 7,
+        )
+        .toLowerCase();
+
+      return formattedURL === formattedTitle;
+    });
+    matchedPlace ? setCurrentPlace(matchedPlace) : unsetCurrentPlace();
+  });
 </script>
 
 <main id="main" class="h-full">
