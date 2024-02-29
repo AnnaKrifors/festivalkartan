ed07cc8788a6fbe5e9d1d35d4aff10a4b8aa46db
Isac Zetterström <113009613+IsacZetterstrom@users.noreply.github.com>
2024-02-29 14:19:25 +0100

    Feature/113 createEnv (#71)
    * created a ENV file with the URL.
    
    * changed the URL so its fetched from the .env file

--- src/data/fetch.ts
+++ src/data/fetch.ts
@@ -1,8 +1,5 @@
-const BASE_URL =
-  "https://test.vasterascityfestival.se/wp-json/places/v1/places/";
-
 export const get = async <T>(url: string) => {
-  const response = await fetch(BASE_URL + url);
+  const response = await fetch(import.meta.env.VITE_URL + url);
   const data = await response.json();
   return data as T;
 };
