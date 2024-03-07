e93c791f1b4a76731e0b8c01504140948c048125
Isac Zetterström <113009613+IsacZetterstrom@users.noreply.github.com>
2024-03-07 12:47:26 +0100

    added styling to each element to override other styling. (#79)
    Added margin 0 aswell as it was added to the elements when live.

--- src/lib/PlaceDetails/Footer.svelte
+++ src/lib/PlaceDetails/Footer.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 </script>
 
-<footer
-  class="flex flex-col gap-2 bg-darkgreen py-8 text-center font-medium tracking-widest text-white"
->
-  <div class="text-xs">
+<footer class="bg-darkgreen flex flex-col gap-2 py-8 text-center text-white">
+  <div class="m-0 text-xs font-medium tracking-widest">
     <p>VÄSTERÅS CITYFESTIVAL</p>
     <p>ARRANGERAS AV:</p>
   </div>
-  <p class="text-base">VISIT VÄSTERÅS</p>
-  <p class="text-xs">EN DEL AV VÄSTERÅS STAD</p>
+  <p class="m-0 text-base font-medium tracking-widest">VISIT VÄSTERÅS</p>
+  <p class="m-0 text-xs font-medium tracking-widest">EN DEL AV VÄSTERÅS STAD</p>
 </footer>
